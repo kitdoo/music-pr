a5c128501d18d4de70dca83f28402066007142bc
Kitdoo <ira.bondarevskaya@gmail.com>
2019-02-13 12:07:19 +0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	//slide2id - плавная прокрутка по ссылкам внутри страницы
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
+
+	
+});
