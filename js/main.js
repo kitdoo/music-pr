9cad8304c579f102b6e74c6095c2a42700a8e12d
Kitdoo <ira.bondarevskaya@gmail.com>
2019-02-13 15:09:01 +0700

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+
+
+
+	//Вызов слайдера
+
+	$("#top-slider").owlCarousel({
+		singleItem: true,
+		navigation: true,
+		theme: "top-slider-theme",
+		navigationText : ["",""],
+		slideSpeed: 600
+	});
+
 	//slide2id - плавная прокрутка по ссылкам внутри страницы
 	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
 	    highlightSelector:"nav a"
